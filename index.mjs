84b719c7b3740e9e983c06366f4b8136813e5aa3
Pascal Ziener <30428982+pascalziener@users.noreply.github.com>
2024-07-21 23:42:31 +0200

    IMPROVE: add welcome header

--- index.mjs
+++ index.mjs
@@ -1,7 +1,19 @@
 #!/usr/bin/env node
-import clearAnyConsole from 'clear-any-console'
 
-clearAnyConsole()
+import cliWelcome from 'cli-welcome'
+import pkgJSON from './package.json' with { "type": "json" }
+
+
+cliWelcome({
+  title: pkgJSON.name,
+  description: pkgJSON.description,
+  version: pkgJSON.version,
+  bgColor: `#FADC00`,
+	color: `#000000`,
+	bold: true,
+	clear: true,
+})
+
 console.log(`
   Pascal Ziener - Software Engineer
 
